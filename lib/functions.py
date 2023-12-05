7ee792e73b6aad316e60ca5b6ba63dd837a08f93
Godfrey Karugu <karugugk@gmail.com>
2023-12-05 21:30:47 +0300

    completed the lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello programmer!")
+ 
+greet_programmer()   
+
+
 
 def greet(name):
-    pass
+    print(f"Hello,{name}")
+
+greet("James")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hi, {name}")
+
+greet_with_default()    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+   
+
+
+
+print(add(5,8))
 
 def halve(number):
-    pass
+    return number/2
+
+print(halve(12))
+
+
